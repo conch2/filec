da57f978d98d95d8eb2a36600f438d19e771dc1d
conch2 <74294886+conch2@users.noreply.github.com>
2021-01-12 11:47:00 +0000

    Update urls.cpp

--- Myhttpd/Http0.1/urls.cpp
+++ Myhttpd/Http0.1/urls.cpp
@@ -8,9 +8,6 @@ std::vector<UrlFunction> urlpatterns =
 	UrlFunction("/", home),
 	UrlFunction("/favicon.ico", ico),
 	UrlFunction("/home", home),
-	UrlFunction("/100.jpg", image),
-	UrlFunction("/110.jpg", image2),
-	UrlFunction("/120.jpg", image3),
 };
 
 void home(SOCKET client, std::string request, ...)
@@ -21,76 +18,10 @@ void home(SOCKET client, std::string request, ...)
 	sendFile(client, GET_TEMPLATES_PATH + "\\home.html");
 }
 
-void image(SOCKET client, std::string request, ...)
-{
-	std::string buf, path = GET_STATIC_PATH + "\\images\\100.jpg";
-	char ch[64] = {};
-
-	buf.assign("HTTP/1.1 200 OK\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign(SERVER_STRING);
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Accept-Ranges: bytes\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Content-Length: ");
-	sprintf(ch, "%lld\r\n", getFileSize(path));
-	buf.append(ch);
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Content-Type: image/jpg\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	send(client, "\r\n", 2, 0);
-
-	sendFile(client, path);
-}
-
-void image2(SOCKET client, std::string request, ...)
-{
-	std::string buf, path = GET_STATIC_PATH + "\\images\\110.jpg";
-	char ch[64] = {};
-
-	buf.assign("HTTP/1.1 200 OK\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign(SERVER_STRING);
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Accept-Ranges: bytes\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Content-Length: ");
-	sprintf(ch, "%lld\r\n", getFileSize(path));
-	buf.append(ch);
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Content-Type: image/jpg\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	send(client, "\r\n", 2, 0);
-
-	sendFile(client, path);
-}
-
-void image3(SOCKET client, std::string request, ...)
-{
-	std::string buf, path = GET_STATIC_PATH + "\\images\\120.jpg";
-	char ch[64] = {};
-
-	buf.assign("HTTP/1.1 200 OK\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign(SERVER_STRING);
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Accept-Ranges: bytes\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Content-Length: ");
-	sprintf(ch, "%lld\r\n", getFileSize(path));
-	buf.append(ch);
-	send(client, buf.c_str(), buf.length(), 0);
-	buf.assign("Content-Type: image/jpg\r\n");
-	send(client, buf.c_str(), buf.length(), 0);
-	send(client, "\r\n", 2, 0);
-
-	sendFile(client, path);
-}
-
 void ico(SOCKET client, std::string request, ...)
 {
 	std::string buf, path = GET_STATIC_PATH + "\\images\\favicon.ico";
-	char ch[64] = {};
+	std::stringstream ss;
 
 	buf.assign("HTTP/1.1 200 OK\r\n");
 	send(client, buf.c_str(), buf.length(), 0);
@@ -99,8 +30,8 @@ void ico(SOCKET client, std::string request, ...)
 	buf.assign("Accept-Ranges: bytes\r\n");
 	send(client, buf.c_str(), buf.length(), 0);
 	buf.assign("Content-Length: ");
-	sprintf(ch, "%lld\r\n", getFileSize(path));
-	buf.append(ch);
+	ss << getFileSize(path) << "\r\n";
+	buf.append(ss.str());
 	send(client, buf.c_str(), buf.length(), 0);
 	buf.assign("Content-Type: image/ico\r\n");
 	send(client, buf.c_str(), buf.length(), 0);
